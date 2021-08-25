c7bfe5b17b979e54a65343db403521f52930c2a4
Rafg2 <89493465+Rafg2@users.noreply.github.com>
2021-08-24 19:28:27 -0500

    Add files via upload

---
+++ Main. C.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+   int number1;
+   int number2;
+   int number3;
+  
+ printf("bienvenidos este software permite sumar tres numeros -ingrese el primer numero \n");
+
+ scanf("%d", &number1);
+
+printf("ingrese segundo numero \n");
+
+ scanf("%d", &number2);
+
+printf("ingrese tercer numero \n");
+
+scanf("%d", &number3);
+
+ printf("el resultado es: %d", number1 + number2 + number3);
+
+ return 0;
+
+}
+
